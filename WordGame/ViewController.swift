d6875406ed6b5d95d2d6c500a343cef785632e2b
Rob Ranf <robranf@gmail.com>
2020-11-18 15:10:39 -0800

    init

--- WordGame/ViewController.swift
+++ WordGame/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    // By using try? in our startWords function we are basically
+    // turning this function into a function
+    // that returns an optional and saying call this code and
+    // if it throws an error, just send back nil instead vs try!
+    // which force unwraps the return causing a crash if it errors
+    // e.g. in this instance in case startWordsURL returns nil
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
     }
-
-
 }
-
